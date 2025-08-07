85daa299b7798405d4f10ca8c19ce65470297ff9
Lorenzo Latta <lorenzo.latta@siaemic.com>
2025-08-07 15:47:44 +0200

    Fixing reading environment parameter from file

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 // Examples of DB connection
-// const db_config_mysql = { user: 'root', password: 'mypass', host: "localhost", port: 3306, dialect: "mysql", db_name: "nep_db" };
-let db_config_mariaDB = { user: 'root', password: 'mypass', host: "localhost", port: 3306, dialect: "mariadb", db_name: "nep_db" };
-// const db_config_posgres = { user: 'postgres', password: 'mypass', host: "localhost", port: 3308, dialect: "postgres", db_name: "nep_db" };
+let db_config_default = { user: 'root', password: 'mypass', host: "localhost", port: 3306, dialect: "mariadb", db_name: "nep_db" };
 const db_config_sqlLite = { user: 'root', password: 'mypass', dialect: "sqlite" }
 
 const logger = require('./service/LoggingService.js').getLogger();
@@ -53,31 +51,31 @@ let dbConfig = db_config_sqlLite;
 if (process.env.DB && process.env.DB.toLowerCase() === "true") {
     logger.warn("Working using external DB");
     if (process.env.USER) {
-        db_config_mariaDB.user = process.env.USER;
+        db_config_default.user = process.env.USER;
     }
     if (process.env.PASSWORD) {
         let decPass = atob(process.env.PASSWORD);
-        db_config_mariaDB.password = decPass;
+        db_config_default.password = decPass;
     }
     if (process.env.HOST) {
-        db_config_mariaDB.host = process.env.HOST;
+        db_config_default.host = process.env.HOST;
     }
     if (process.env.PORT) {
         try {
-            db_config_mariaDB.port = parseInt(process.env.PORT);
+            db_config_default.port = parseInt(process.env.PORT);
         } catch (e) {
-            db_config_mariaDB.port = 3306;
+            db_config_default.port = 3306;
             logger.warn("Using default port for DB");
         }
-        db_config_mariaDB.port = process.env.PORT;
+        db_config_default.port = process.env.PORT;
     }
     if (process.env.DIALECT) {
-        db_config_mariaDB.dialect = process.env.DIALECT;
+        db_config_default.dialect = process.env.DIALECT;
     }
     if (process.env.DB_NAME) {
-        db_config_mariaDB.db_name = process.env.DB_NAME;
+        db_config_default.db_name = process.env.DB_NAME;
     }
-    dbConfig = db_config_mariaDB;
+    dbConfig = db_config_default;
 } else {
     logger.warn("No DB selected, using by default sqlite");
 }
