ec0afaf6db7d64cdbdf4472485a40dacb7fc54d2
Lorenzo Latta <lorenzo.latta@siaemic.com>
2026-03-09 10:13:05 +0100

    Fix the log for BasicServicesService

--- server/service/BasicServicesService.js
+++ server/service/BasicServicesService.js
@@ -87,10 +87,14 @@ async function processMountNamesInBatches(mountNameList, requestHeaders, taskTra
   const errors = [];
   let index = 0;
 
+  logger.info(`Doing Cyclic process with:`);
+  logger.info(`- Sliding Windows: ${MAX_CONCURRENT}`);
+  logger.info(`- Max Req Timeout: ${MAX_TIMEOUT}ms`);
+  logger.info(`- N Of Retries: ${N_OF_RETRIES}`);
+  logger.info(`- Delay Retry: ${DELAY_RETRY}ms`);
+
   if (global.throttle && global.throttle == true) {
-    logger.info(`Doing Cyclic process with:\n- Max Concurrent: ${MAX_CONCURRENT}\n- Max Timeout: ${MAX_TIMEOUT}ms\n- N Of Retries: ${N_OF_RETRIES}\n- Delay Retry: ${DELAY_RETRY}ms\n- Waiting Time Between CC Retrievals: ${TIME_BTW_CC_RETRIVALS}ms`);
-  } else {
-    logger.info(`Doing Cyclic process with:\n- Max Concurrent: ${MAX_CONCURRENT}\n- Max Timeout: ${MAX_TIMEOUT}ms\n- N Of Retries: ${N_OF_RETRIES}\n- Delay Retry: ${DELAY_RETRY}ms`);
+    logger.info(`- Waiting Time Between CC Retrievals: ${TIME_BTW_CC_RETRIVALS}ms`);
   }
 
   async function delay(ms) {
