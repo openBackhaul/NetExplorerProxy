241dbbdac8d764d63c850a85be766da22844aab2
Ramineedi Venkata Sravani <ramineedi.venkata-sravani@capgemini.com>
2026-03-19 12:39:50 +0530

    Prioritize the list of Mountnames based on the new mountNames added.

--- server/service/BasicServicesService.js
+++ server/service/BasicServicesService.js
@@ -9,7 +9,6 @@ const ltpStructureUtility = require('./LtpStructureUtility');
 
 const dbHandler = require('./db/dbHandler.js');
 const processMountNames=require('./ProcessMountNames.js');
-
 const logger = require('./LoggingService.js').getLogger();
 
 
@@ -213,14 +212,17 @@ module.exports.embedYourself = async function embedYourself(body, user, xCorrela
       traceIndicator,
       customerJourney
     };
-
-    //call for offline mountName processiong 
-    await processMountNames.addNewDataInNEPdeviceList(mountNameList);
-
+     
+const dbMountNames = (await dbHandler.readListOfDevices()).map(d => d['mount-name']);
+const newMounts = mountNameList.filter(m => !dbMountNames.includes(m));
+const existingMounts = mountNameList.filter(m => dbMountNames.includes(m));
+const prioritizedMountNames = [...newMounts, ...existingMounts];
+await processMountNames.addNewDataInNEPdeviceList(prioritizedMountNames);
+   
     // Call the batch processor here
     traceIncrement += 1;
     await processMountNamesInBatches(
-      mountNameList, requestHeaders, traceIncrement++, timestamp
+      prioritizedMountNames, requestHeaders, traceIncrement++, timestamp
     );
   } else {
     logger.warn(listOfConnectedDevices, "List of connected device is empty or wrong");
@@ -228,7 +230,7 @@ module.exports.embedYourself = async function embedYourself(body, user, xCorrela
 };
 
 module.exports.doWorkThread = async function doWorkThread(requestHeaders, traceIndicatorIncrementer, mountName, timestamp) {
-  let ccOfMountName = await getDataFromOtherApp.retriveTheCC(requestHeaders, traceIndicatorIncrementer, mountName);
+let ccOfMountName = await getDataFromOtherApp.retriveTheCC(requestHeaders, traceIndicatorIncrementer, mountName);
   // Calculate Timestamp when data is retrieved from MWDI
   let newTimeStamp = Date.now();
   logger.debug(`Data retrieved from MWDI for Mountname: ${mountName} at the time: ${newTimeStamp}`);
@@ -244,7 +246,7 @@ module.exports.processTheccOfMountname = async function processTheccOfMountname(
     processEthernetContainergeneralInfo(ccOfMountname, mountName, timestamp);
     processWireInterfaceGeneralInfo(ccOfMountname, mountName, timestamp);
     processEquipmentGeneralInfo(ccOfMountname, mountName, timestamp);
-    // Add processLtpEquipmentMappings(ccOfMountname, mountName, timestamp); // From NEP 1.2.0
+ //   processLtpEquipmentMappings(ccOfMountname, mountName, timestamp); // From NEP 1.2.0
     await processAirContainerGeneralInfoAndTransmissionInfo(ccOfMountname, mountName, timestamp);
     logger.info(`Data has been commited in the DB for Mount-Name: ${mountName}`);
   } else {
