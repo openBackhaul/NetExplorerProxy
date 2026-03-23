1f80893633faeab7b582135376c4e9dbcf578008
KAYAKU <kubilay.kaya@siaemic.com>
2026-03-23 11:46:53 +0100

    server/tests folder is cleaned

--- server/tests/scenarios/00-prerun-embed-yourself.test.js
+++
@@ -1,8 +0,0 @@
-const { ensureDataLoaded } = require('./common/liveApiDbHelper');
-
-describe('Test 0: Pre-run embed-yourself', () => {
-  test('ensures NEP has loaded data before other API tests', async () => {
-    await ensureDataLoaded({ allowEmbed: true });
-  });
-});
-
--- server/tests/setup/orderedSequencer.js
+++
@@ -1,9 +0,0 @@
-const TestSequencer = require('@jest/test-sequencer').default;
-
-class OrderedSequencer extends TestSequencer {
-  sort(tests) {
-    return tests.sort((a, b) => a.path.localeCompare(b.path));
-  }
-}
-
-module.exports = OrderedSequencer;
